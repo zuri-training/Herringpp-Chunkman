2f4ca22c016628613c9673ba55079096344e3f99
Praise Jones <praisejones18@gmail.com>
2022-12-11 20:58:37 +0100

    update

--- Back-end/app.js
+++ Back-end/app.js
@@ -2,10 +2,21 @@ require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
 
+// importing user context
+const User = require("./model/user");
+
 const app = express();
 
 app.use(express.json());
 
-// Logic goes here
+// Register
+app.post("/sign-up", (req, res) => {
+    //register logic goes here...
+    });
+    
+    // Login
+    app.post("/sign-in", (req, res) => {
+    //login logic goes here
+    });
 
 module.exports = app;

ef12289ac51ddbb4a7431c36693002da755e2092
Praise Jones <praisejones18@gmail.com>
2022-12-11 20:54:21 +0100

    update

--- Back-end/index.js
+++ Back-end/index.js
@@ -8,4 +8,4 @@ const port = process.env.PORT || API_PORT;
 // server listening 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
-})
+}) 
--- Back-end/users/user.js
+++ Back-end/users/user.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
-const toDoSchema = new mongoose.Schema({
-	title: { type: String, required: true },
-	description: { type: String, required: true },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema({
+  first_name: { type: String, default: null },
+  last_name: { type: String, default: null },
+  email: { type: String, unique: true },
+  password: { type: String },
+  token: { type: String },
+});
 
-
-module.exports = mongoose.model("ToDo", toDoSchema);
+module.exports = mongoose.model("user", userSchema);

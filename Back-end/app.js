4e5649640316cea71b8a967f0e5bb92ecba79c18
Praise Jones <praisejones18@gmail.com>
2022-12-11 20:43:44 +0100

    modify

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,27 +1,11 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-require('dotenv/config')
+require("dotenv").config();
+require("./config/database").connect();
+const express = require("express");
 
+const app = express();
 
-// route starts
-app.get('/', (req, res) => {
-    res.render("index.ejs")
-})
+app.use(express.json());
 
-app.get('/sign-up', (req, res) => {
-    res.render("sign-up.ejs")
-})
+// Logic goes here
 
-app.get('/sign-in', (req, res) => {
-    res.render("sign-in.ejs")
-})
-// end routes
-
-
-mongoose.connect(process.env.DB_CONNECTION, () => {
-        console.log('connected')
-    }
-);
-
-app.listen(3000)
+module.exports = app;

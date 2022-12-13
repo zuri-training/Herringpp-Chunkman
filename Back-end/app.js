097c3558b987656ff160dd507627a715f6254588
Praise Jones <praisejones18@gmail.com>
2022-12-13 10:29:12 +0100

    added CORS

--- Back-end/app.js
+++ Back-end/app.js
@@ -5,10 +5,13 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 // importing user context
+const cors = require("cors") 
 const User = require("./model/user");
 const app = express();
 
-app.use(express.json());
+app.use(cors())
+
+app.use(express.json({ limit: "50mb" }));
 
 // signup
 app.post("/sign-up", async (req, res) => {
@@ -91,6 +94,12 @@ app.post("/sign-in", async (req, res) => {
   } catch (err) {
     console.log(err);
   }
+});
+
+const auth = require("./middleware/auth");
+
+app.post("/home", auth, (req, res) => {
+  res.status(200).send("Welcome to Chunkit");
 });
 
 module.exports = app;

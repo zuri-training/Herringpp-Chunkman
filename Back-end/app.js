24b3ac07599186021f0add04e57194da72776cee
Tayo Akosile <oluwatayoakosile@gmail.com>
2022-12-14 16:53:02 +0100

    fixed untracked files

---
+++ Back-end/app.js
@@ -0,0 +1,105 @@
+require("dotenv").config();
+require("./config/database").connect();
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const auth = require("./middleware/auth");
+
+// importing user context
+const cors = require("cors") 
+const User = require("./model/user");
+const app = express();
+
+app.use(cors())
+
+app.use(express.json({ limit: "50mb" }));
+
+// signup
+app.post("/sign-up", async (req, res) => {
+  try {
+    // Get user input
+    const { firstName, lastName, email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password && firstName && lastName)) {
+      res.status(400).send("All input is required");
+    }
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
+    //Encrypt user password
+    encryptedUserPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name: firstName,
+      last_name: lastName,
+      email: email.toLowerCase(), // sanitize
+      password: encryptedUserPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "6h",
+      }
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// signin
+app.post("/sign-in", async (req, res) => {
+  try {
+    //user input
+    const { email, password } = req.body;
+
+    // Check if any of the input is blank.
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate user
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "6h",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      // sending back user
+      res.status(200).json(user);
+    }
+    res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/home", auth, (req, res) => {
+  res.status(200).send("Welcome to Chunkit");
+});
+
+module.exports = app;

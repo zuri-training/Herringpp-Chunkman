20db55e3c82523ed452bc824b011f844fb55d0de
Praise Jones <praisejones18@gmail.com>
2022-12-17 23:05:34 +0100

    modified

--- Back-end/model/user.js
+++ Back-end/model/user.js
@@ -3,21 +3,21 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   fullName: { 
     type: String, 
-    required: true,
-    min: 6,
-    max: 50
+    // required: true,
+    // min: 6,
+    // max: 50
   },
   email: { 
     type: String, 
-    unique: true,
-    min: 6,
-    max: 50 
+    // unique: true,
+    // min: 6,
+    // max: 50 
   },
   password: { 
     type: String, 
-    required: true,
-    min: 6,
-    max: 1120
+    // required: true,
+    // min: 6,
+    // max: 1120
   },
   token: { 
     type: String 

c234756026dadda2bf888f285247b19d675775e6
Weirde2020 <sambukks18@gmail.com>
2022-12-19 21:00:34 +0100

    Here is an update containing the rest of the files

--- Frontend/src/Dashboard/dashboard.js
+++ Frontend/src/Dashboard/dashboard.js
@@ -1,9 +1,21 @@
 
  // Get the links
- var historyLink = document.getElementById("history-link");
- var shareLink = document.getElementById("share-link");
+ function showHistory() {
+  // Show the history panel
+  var historyPanel = document.getElementById("history-panel");
+  historyPanel.style.display = "block";
+} var shareLink = document.getElementById("share-link");
  var ratingsLink = document.getElementById("ratings-link");
-
+ 
+ function toggleNotifications() {
+  // Toggle the visibility of the notifications panel
+  var notificationsPanel = document.getElementById("notifications-panel");
+  if (notificationsPanel.style.display === "none") {
+    notificationsPanel.style.display = "block";
+  } else {
+    notificationsPanel.style.display = "none";
+  }
+}
  // Add click event listeners to the links
  historyLink.addEventListener("click", function() {
    // code to handle the click event on the "History" link goes here

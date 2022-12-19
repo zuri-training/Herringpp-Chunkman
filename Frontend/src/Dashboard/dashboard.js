04f9d9b85fab19d4eed091b49627cda8526e2488
Weirde2020 <sambukks18@gmail.com>
2022-12-19 14:04:48 +0100

    an updated version of the javascript file

--- Frontend/src/Dashboard/dashboard.js
+++ Frontend/src/Dashboard/dashboard.js
@@ -1,7 +1,25 @@
-const username = "user";
-const email = "user@email.com";
 
+ // Get the links
+ var historyLink = document.getElementById("history-link");
+ var shareLink = document.getElementById("share-link");
+ var ratingsLink = document.getElementById("ratings-link");
 
-function toggleNotifications() {
-  // Code to toggle the visibility of the notifications goes here
-}
+ // Add click event listeners to the links
+ historyLink.addEventListener("click", function() {
+   // code to handle the click event on the "History" link goes here
+ });
+ shareLink.addEventListener("click", function() {
+   // code to handle the click event on the "Share" link goes here
+ });
+ ratingsLink.addEventListener("click", function() {
+   // code to handle the click event on the "Ratings" link goes here
+ });
+     // Get the user name element
+     var userNameElement = document.getElementById("user-name");
+
+     // Get the user name from a server or from some other source
+     var userName = "John";
+ 
+     // Set the user name in the element
+     userNameElement.textContent = "Hello " + userName;
+ 

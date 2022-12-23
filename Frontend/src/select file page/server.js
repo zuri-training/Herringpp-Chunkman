6536e00fe13da0bdc66bc49fc373fe5b5d0ac489
Praise Jones <praisejones18@gmail.com>
2022-12-23 13:16:22 +0100

    updated

--- Frontend/src/select file page/server.js
+++ Frontend/src/select file page/server.js
@@ -4,20 +4,20 @@ const formData = new FormData();
 formData.append('file', file); // file is the JSON file to be uploaded
 
 
-async function fetchAPI(endpoint, method){
-    try{
+async function fetchAPI(data, endpoint, method){
+    try {
         const response = await fetch(`${API}/${endpoint}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            },
-            body: formData
-        }).then(response => {
-            return response.json();
-          }).then(data => {
-            console.log(data);
-        })
-    } catch (error) {
-        console.log(error);
-    }
+          method: 'POST',
+          body: formData,
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+
+        const result = await response.json();
+        return result; // The chunks are returned in the response data
+      } catch (error) {
+        console.error(error);
+      }
+      
 }
